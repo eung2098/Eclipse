e7f54f54f4ae40066351c307a678f34b184328c9
eung2098 <jeg8421@naver.com>
2021-10-06 18:54:07 +0900

    Add Java File [OperatorTest.java]

---
+++ OperatorTest.java
@@ -0,0 +1,20 @@
+package ch11;
+
+public class OperatorTest {
+
+	public static void main(String[] args) {
+
+		int gameScore = 150;
+		
+		int lastScore = gameScore--;  //gmaeScore += 1;  gmaeScore = gameScore +1;
+		
+		System.out.println(lastScore);
+		System.out.println(gameScore);
+		
+		//num1 += 2; num1 + 2;
+		//num1 *= 2; num1 * 2;
+		
+		
+	}
+
+}
