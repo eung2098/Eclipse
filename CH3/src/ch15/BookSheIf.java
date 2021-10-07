a7771686e833c0b664f076e69c7413af801634b1
eung2098 <jeg8421@naver.com>
2021-10-07 16:25:52 +0900

    Add Java File [interface implements]

---
+++ CH3/src/ch15/BookSheIf.java
@@ -0,0 +1,20 @@
+package ch15;
+
+public class BookSheIf extends SheIf implements Queue {
+
+	@Override
+	public void enQueue(String title) {
+		sheIf.add(title);
+	}
+
+	@Override
+	public String deQueue() {
+		return sheIf.remove(0);
+	}
+
+	@Override
+	public int getSize() {
+		return getCount();
+	}
+
+}
